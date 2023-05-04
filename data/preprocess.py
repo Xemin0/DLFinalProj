5bcd5dfc5fba78bc3668d03c26e969c19f6d2069
Xemin0 <shilai_yang@brown.edu>
2023-05-04 14:37:48 -0600

    fixed preprocess

--- data/preprocess.py
+++ data/preprocess.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 
 def TrainDatasetFromFolder():
-    dataset_dir = "data/DIV2K_train_HR/DIV2K_train_HR/"
+    dataset_dir = "./DIV2K_train_HR/DIV2K_train_HR/"
     image_filenames = [join(dataset_dir, i) for i in listdir(dataset_dir)]
     lr_images = []
     hr_images = []
@@ -17,30 +17,29 @@ def TrainDatasetFromFolder():
     upscale_factor = 4
 
     crop_size = crop_size - (crop_size % upscale_factor)
-    
+    #print('crop_size', crop_size)
     for image_filename in image_filenames:
-        print(image_filename)
+        #print(image_filename)
         hr_image = tf.keras.utils.load_img(image_filename)
         hr_image = tf.convert_to_tensor(hr_image)
-        print(hr_image)
+        #print(hr_image.shape)
         
-        hr_transform = tf.image.random_crop(hr_image, size = [crop_size,crop_size,3])
-        lr_transform = tf.image.resize(hr_transform, crop_size // upscale_factor)
+        hr_transform = tf.image.random_crop(hr_image, size = [crop_size,crop_size, hr_image.shape[-1]])
+        lr_transform = tf.image.resize(hr_transform, size = (crop_size // upscale_factor, crop_size // upscale_factor))
         
         lr_images.append(hr_transform)
         hr_images.append(lr_transform)
-        print("hello")
-        print(hr_transform)
-        return 
+        #print("hello")
+        #print(hr_transform)
 
     lr_images = tf.stack(lr_images)
     hr_images = tf.stack(hr_images)
-    print(lr_images.shape)
-    print(lr_images[0].shape)
+    #print(lr_images.shape)
+    #print(lr_images[0].shape)
 
     return lr_images, hr_images
 
-TrainDatasetFromFolder()
+#TrainDatasetFromFolder()
 
 
 
