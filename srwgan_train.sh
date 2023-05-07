e1e1516d574f53daa5b3a4256d6a353fca233749
Xemin0 <shilai_yang@brown.edu>
2023-05-07 15:09:51 -0600

    change SLURMs script

--- srwgan_train.sh
+++ srwgan_train.sh
@@ -34,7 +34,7 @@ nvidia-smi
 export PYTHONUNBUFFERED=TRUE
 
 # Load modules 
-#module load openssl/3.0.0 cuda/11.7.1 cudnn/8.2.0
+#module load python/3.11.0 openssl/3.0.0 cuda/11.7.1 cudnn/8.2.0
 
 # Activate Conda Env
 #conda activate /anaconda3/envs/srwgan
